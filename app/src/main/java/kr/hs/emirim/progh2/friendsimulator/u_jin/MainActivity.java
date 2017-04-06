e19f69c69d24ab164abffea7faa3b88e9f52747a
chasu1228 <chasu1228@naver.com>
2017-04-06 20:16:34 +0900

    #8 첫번째 버튼 동작하게 하기 -> onClick

--- app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
@@ -1,7 +1,8 @@
 package kr.hs.emirim.progh2.friendsimulator.u_jin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    void onClick(int view){
+        Toast.makeText(this,"작동 해요~",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
