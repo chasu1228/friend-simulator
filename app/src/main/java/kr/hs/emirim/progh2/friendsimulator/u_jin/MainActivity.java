8091cdc74cd48746b7f3aa95d26c25bf2cf65f53
chasu1228 <chasu1228@naver.com>
2017-04-06 20:37:16 +0900

    #9 여러개버튼 동작완료

--- app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.progh2.friendsimulator.u_jin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +13,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void onClick(int view){
-        Toast.makeText(this,"작동 해요~",Toast.LENGTH_SHORT).show();
-    }
+    void onClick(int view) {
+        switch (view.getId()){
+            case R.id.button1 :
+                Toast.makeText(this, "뚜뚜들아~", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button2:
+                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button3:
+                Toast.makeText(this, "티티", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button4:
+                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button5:
+                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+        }
 
+        }
+    }
 
 }
