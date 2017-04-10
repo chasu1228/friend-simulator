4d2a2911ded8ab993a65e3026b712d681aea92ce
chasu1228 <chasu1228@naver.com>
2017-04-10 20:38:03 +0900

    #13 유진사진프로젝트추가

---
+++ app/src/androidTest/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivityTest.java
@@ -0,0 +1,19 @@
+package kr.hs.emirim.progh2.friendsimulator.u_jin;
+
+import org.junit.Test;
+
+/**
+ * Created by mirim on 2017-04-10.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onClick() throws Exception {
+
+    }
+
+}
--- app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
@@ -2,32 +2,42 @@ package kr.hs.emirim.progh2.friendsimulator.u_jin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG="장유진 : MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreats 메소드 호출~");
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"activity_main 레이아웃 세팅~");
+
     }
 
     void onClick(int view) {
+        Log.d(TAG,"onCreats 메소드 호출~");
+        Log.d(TAG,"클릭된 뷰 id : "+view.getId());
         switch (view.getId()){
-            case R.id.button1 :
+            case R.id.button1:
+                Log.d(TAG,"버튼1 클릭");
                 Toast.makeText(this, "뚜뚜들아~", Toast.LENGTH_SHORT).show(); break;
             case R.id.button2:
+                Log.d(TAG,"버튼2 클릭");
                 Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
             case R.id.button3:
+                Log.d(TAG,"버튼3 클릭");
                 Toast.makeText(this, "티티", Toast.LENGTH_SHORT).show(); break;
             case R.id.button4:
+                Log.d(TAG,"버튼4 클릭");
                 Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
             case R.id.button5:
+                Log.d(TAG,"버튼5 클릭");
                 Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
         }
 
         }
     }
-
-}
