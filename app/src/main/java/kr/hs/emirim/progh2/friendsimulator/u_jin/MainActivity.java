631d17dca2bcc724afd3ce0b6f200a8f1b860685
chasu1228 <chasu1228@naver.com>
2017-04-10 20:54:24 +0900

    #12 평소상태(일껄?) 이미지 넣고 참조 만들기

--- app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/u_jin/MainActivity.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG="장유진 : MainActivity";
+    private ImageView mImageViewfriendVisual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"onCreats 메소드 호출~");
         setContentView(R.layout.activity_main);
+        mImageViewfriendVisual = (ImageView) findViewById(R.id.friend_visual);
         Log.d(TAG,"activity_main 레이아웃 세팅~");
 
     }
 
-    void onClick(int view) {
+    void onClick( View view) {
         Log.d(TAG,"onCreats 메소드 호출~");
         Log.d(TAG,"클릭된 뷰 id : "+view.getId());
         switch (view.getId()){
@@ -27,16 +30,16 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "뚜뚜들아~", Toast.LENGTH_SHORT).show(); break;
             case R.id.button2:
                 Log.d(TAG,"버튼2 클릭");
-                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+                Toast.makeText(this, "마자미야~", Toast.LENGTH_SHORT).show(); break;
             case R.id.button3:
                 Log.d(TAG,"버튼3 클릭");
                 Toast.makeText(this, "티티", Toast.LENGTH_SHORT).show(); break;
             case R.id.button4:
                 Log.d(TAG,"버튼4 클릭");
-                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+                Toast.makeText(this, "난 모르겠는걸..", Toast.LENGTH_SHORT).show(); break;
             case R.id.button5:
                 Log.d(TAG,"버튼5 클릭");
-                Toast.makeText(this, "작동 해요~", Toast.LENGTH_SHORT).show(); break;
+                Toast.makeText(this, "왜여~", Toast.LENGTH_SHORT).show(); break;
         }
 
         }
